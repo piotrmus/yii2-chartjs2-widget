067481aa7b4406f16cbad6eb4afe0cbd96c5c914
Piotr Musiał <Piotr Musia>
2016-07-05 21:18:04 +0200

    Init commit

---
+++ src/ChartJs2.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @link https://github.com/piotrmus/yii2-chartjs2-widget
+ * @copyright Copyright (c) 2016 Piotr Musiał
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+namespace piotrmus\chartjs2;
+
+use yii\base\InvalidConfigException;
+use yii\base\Widget;
+use yii\helpers\Html;
+use yii\helpers\Json;
+
+/**
+ *
+ * Chart renders a canvas ChartJs2 plugin widget.
+ *
+ * @author Piotr Musiał <piotr.mus@gmail.com>
+ * @link http://www.codehat.pl/
+ */
+class ChartJs2 extends Widget
+{
+    public function init()
+    {
+        parent::init();
+
+    }
+
+    public function run()
+    {
+
+    }
+}
